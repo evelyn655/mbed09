9487a7f8963a84994b7a8ac57ec87500b1b00abc
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-04-21 03:14:36 -0700

    add

--- 9_3_Custom_RPC/main.cpp
+++ 9_3_Custom_RPC/main.cpp
@@ -9,8 +9,14 @@ RpcDigitalOut myled1(LED1,"myled1");
 RpcDigitalOut myled2(LED2,"myled2");
 RpcDigitalOut myled3(LED3,"myled3");
 BufferedSerial pc(USBTX, USBRX);
-void LEDControl(Arguments *in, Reply *out);
-RPCFunction rpcLED(&LEDControl, "LEDControl");
+void LEDControl_ORANGE(Arguments *in, Reply *out);
+RPCFunction rpcLED_ORANGE(&LEDControl_ORANGE, "LEDControl_ORANGE");
+void LEDControl_GREEN(Arguments *in, Reply *out);
+RPCFunction rpcLED_GREEN(&LEDControl_GREEN, "LEDControl_GREEN");
+void BLINK(Arguments *in, Reply *out);
+RPCFunction rpcBLINK(&BLINK, "BLINK");
+
+
 double x, y;
 
 int main() {
@@ -37,7 +43,7 @@ int main() {
         printf("%s\r\n", outbuf);
     }
 }
-
+/*
 // Make sure the method takes in Arguments and Reply objects.
 void LEDControl (Arguments *in, Reply *out)   {
     bool success = true;
@@ -59,4 +65,92 @@ void LEDControl (Arguments *in, Reply *out)   {
     } else {
         out->putData("Failed to execute LED control.");
     }
+}
+*/
+
+//demo
+void LEDControl_ORANGE (Arguments *in, Reply *out)   {
+    bool success = true;
+
+    // In this scenario, when using RPC delimit the two arguments with a space.
+    //x = in->getArg<double>();
+    y = in->getArg<double>();
+
+    // Have code here to call another RPC function to wake up specific led or close it.
+    char buffer[200], outbuf[256];
+    char strings[20];
+    //int led = x;
+    int on = y;
+    sprintf(strings, "/myled3/write %d", on);
+    strcpy(buffer, strings);
+    RPC::call(buffer, outbuf);
+    if (success) {
+        out->putData(buffer);
+    } else {
+        out->putData("Failed to execute LED control.");
+    }
+}
+
+void LEDControl_GREEN (Arguments *in, Reply *out)   {
+    bool success = true;
+
+    // In this scenario, when using RPC delimit the two arguments with a space.
+    //x = in->getArg<double>();
+    y = in->getArg<double>();
+
+    // Have code here to call another RPC function to wake up specific led or close it.
+    char buffer[200], outbuf[256];
+    char strings[20];
+    //int led = x;
+    int on = y;
+    sprintf(strings, "/myled/write %d", on);
+    strcpy(buffer, strings);
+    RPC::call(buffer, outbuf);
+    if (success) {
+        out->putData(buffer);
+    } else {
+        out->putData("Failed to execute LED control.");
+    }
+}
+
+
+// void BLINK(Arguments *in, Reply *out)   {
+//     bool success = true;
+
+//     // In this scenario, when using RPC delimit the two arguments with a space.
+//     //x = in->getArg<double>();
+//     //y = in->getArg<double>();
+
+//     // Have code here to call another RPC function to wake up specific led or close it.
+//     char buffer[200], outbuf[256];
+//     char strings[20];
+//     //int led = x;
+//     while(1) {
+//         RPC::call("/myled3/write 1", outbuf);
+//         ThisThread::sleep_for(500ms);
+//         RPC::call("/myled3/write 0", outbuf);
+//         ThisThread::sleep_for(500ms);
+//     }
+    
+// }
+
+
+void BLINK(Arguments *in, Reply *out)   {
+    bool success = true;
+
+    // In this scenario, when using RPC delimit the two arguments with a space.
+    //x = in->getArg<double>();
+    //y = in->getArg<double>();
+
+    // Have code here to call another RPC function to wake up specific led or close it.
+    char buffer[200], outbuf[256];
+    char strings[20];
+    //int led = x;
+    while(1) {
+        RPC::call("/LEDControl_ORANGE/run 1\r", outbuf);
+        ThisThread::sleep_for(500ms);
+        RPC::call("/LEDControl_ORANGE/run 0\r", outbuf);
+        ThisThread::sleep_for(500ms);
+    }
+    
 }
